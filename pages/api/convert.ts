3ca8757d562f24eb33cba913f9ce3bdffc52740e
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-29 11:54:12 +0100

    suprememods

--- pages/api/convert.ts
+++ pages/api/convert.ts
@@ -1,16 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
-async function getData() {
-}
-
 export default async function convert(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const eth_btc_ratio: any = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=btc")
+    const eth_btc_ratio: any = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=btc")
         .then((response: any) => response.json()).then((response: any) => {return response})
-        console.log("hi")
-        res.end(eth_btc_ratio.ethereum.btc.toString())
-    } catch (error) {
-        console.log(error)
-        res.end("0")
-    }
+    res.end(eth_btc_ratio.ethereum.btc.toString())
 }
