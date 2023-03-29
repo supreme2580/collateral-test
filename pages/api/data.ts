153837abff5bd0445a055deee1b616190dfea613
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-03-29 12:56:16 +0100

    suprememods

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -3,9 +3,22 @@ import client from "../../apollo-client"
 import { NextApiRequest, NextApiResponse } from "next"
 const convert = require("ether-converter")
 
+// const eth_btc_ratio = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=btc")
+//         .then((response: any) => response.json()).then((response: any) => {return response})
+//     const result = await client.query({
+//         query: gql`
+//         {
+//             agreementPositions {
+//               balance
+//             }
+//           }
+//         `
+//     }).catch(e => console.log(e))
+
 async function getBalance() {
-    const eth_btc_ratio = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=btc")
+    const prices = await fetch("http://api.coinlayer.com/live?access_key=91244bb417e894c175aa47391c8fbeb1")
         .then((response: any) => response.json()).then((response: any) => {return response})
+    const eth_btc_ratio = prices.rates.ETH/prices.rates.BTC
     const result = await client.query({
         query: gql`
         {
@@ -22,7 +35,7 @@ async function getBalance() {
         const balance = balanceArray.reduce(function(a, b){
             return Number(a) + Number(b)
         }, 0)
-        const convertedBalance = convert(balance, 'wei').ether * eth_btc_ratio.ethereum.btc
+        const convertedBalance = convert(balance, 'wei').ether * eth_btc_ratio
         return JSON.stringify(convertedBalance)
     }
     else {
